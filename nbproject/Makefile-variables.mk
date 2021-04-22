5585fb940157fdd489e04cdb31bc6f8dd611b0f3
20185090 <20185090@DSN1271158634.cfp127aluno.local>
2021-04-22 13:51:23 -0300

    Teste Inicial

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=display_LCD.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/display_LCD.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=displaylcd.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/displaylcd.x.tar
